0679a0cf0b0cf8616339bb2e18ee72c0aa317298
Santrafysh <121214628+Commatecs@users.noreply.github.com>
2024-04-29 21:10:39 +0300

    first commit

---
+++ src/main/java/com/santrafysh/zomboid/ZomboidClient.java
@@ -0,0 +1,10 @@
+package com.santrafysh.zomboid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZomboidClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
